51e7ded32fd570414a52f27b1fd6727e3796dc50
Alexande B <abobrikovich@gmail.com>
2018-10-24 00:09:24 +0300

    Add circle ci 2.0

--- build_ios_fat.sh
+++ build_ios_fat.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# git clone --recurse-submodules https://github.com/socketio/socket.io-client-cpp.git
-# git clone https://github.com/leetal/ios-cmake
-# cd socket.io-client-cpp
-
 export DARWIN_DEPLOYMENT_TARGET=8.0
 
-archs="armv7 armv7s arm64 i386 x86_64"
+build_type=${1:-Release}
+rebuild=${2}
+archs="armv7 armv7s armv8 x86 x86_64"
 
 rm -rf build-ios-*/
 rm libssl.a
@@ -14,25 +12,29 @@ rm libcrypto.a
 rm libboost_*.a
 rm libsioclient_tls.a
 
+if [ ! -d ios-cmake ]; then
+	git clone https://github.com/leetal/ios-cmake
+fi
+
 for arch in $archs; do
 	mkdir -p build-ios-$arch && pushd build-ios-$arch
 
-	conan install .. --profile ~/.conan/profiles/ios-$arch --build
+	conan install .. -s arch=${arch} -s compiler=apple-clang -s compiler.version=9.1 -s os=iOS -s os.version=8.0 -s compiler.libcxx=libc++ -s build_type=${build_type} ${rebuild}
 
 	platform="OS"
-	if [ "$arch" = "i386" ]; then
+	if [ "$arch" = "x86" ]; then
 		platform="SIMULATOR"
 	elif [ "$arch" = "x86_64" ]; then
 		platform="SIMULATOR64"
 	fi
 
-	cmake -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake -DIOS_PLATFORM=$platform ..
+	cmake -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake -DIOS_PLATFORM=$platform -DIOS_DEPLOYMENT_TARGET=8.0 -DENABLE_BITCODE=YES -DCMAKE_BUILD_TYPE=${build_type} ..
 	cmake --build .
 
 	popd
 done
 
-lipo -create build-ios-arm64/libsioclient_tls.a build-ios-x86_64/libsioclient_tls.a build-ios-i386/libsioclient_tls.a -output libsioclient_tls.a
+lipo -create build-ios-armv8/libsioclient_tls.a build-ios-x86_64/libsioclient_tls.a build-ios-x86/libsioclient_tls.a -output libsioclient_tls.a
 
 lipo -create ~/.conan/data/OpenSSL/1.1.1/conan/stable/package/*/lib/libssl.a -output libssl.a
 lipo -create ~/.conan/data/OpenSSL/1.1.1/conan/stable/package/*/lib/libcrypto.a -output libcrypto.a
