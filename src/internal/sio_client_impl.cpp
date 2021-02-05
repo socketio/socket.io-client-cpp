3156a33d4013b1116ed6eebcc21b902f25330cf2
Adrian DC <radian.dc@gmail.com>
2021-02-05 23:25:13 +0100

    fix: resolve client_impl::ping LOG call syntax in debug builds

--- src/internal/sio_client_impl.cpp
+++ src/internal/sio_client_impl.cpp
@@ -302,7 +302,7 @@ namespace sio
         if(ec || m_con.expired())
         {
             if (ec != asio::error::operation_aborted)
-                LOG("ping exit,con is expired?"<<m_con.expired()<<",ec:"<<ec.message()<<endl){};
+                LOG("ping exit,con is expired?"<<m_con.expired()<<",ec:"<<ec.message()<<endl);
             return;
         }
         packet p(packet::frame_ping);
